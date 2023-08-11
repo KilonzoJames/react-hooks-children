6e8ad837cf1d2f49fec22cc843bcf0e5768e373c
KilonzoJames <jay.kilonzo@gmail.com>
2023-08-11 20:10:09 +0300

    Learnt about react children

--- src/components/App.js
+++ src/components/App.js
@@ -2,37 +2,38 @@ import React from "react";
 import Container from "./Container";
 
 function App() {
+  const generateExampleDivs = (count) => {
+    const exampleDivs = [];
+    for (let i = 0; i < count; i++) {
+      exampleDivs.push(<div key={i}>This is an example!</div>);
+    }
+    return exampleDivs;
+  };
+  const fourTurtles=(turtles)=>{
+    const exampleTurtle=[];
+    for(let i=0; i<turtles; i++){
+      exampleTurtle.push(
+        <img
+            key={i}
+            alt="turtle"
+            src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
+        />
+      );
+    }
+      return exampleTurtle;
+   }
+
   return (
     <div className="App">
       <Container header="Example header" direction="horizontal">
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
+      {generateExampleDivs(7)}
       </Container>
       <Container header="Example header" textPosition="center">
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
+        {fourTurtles(7)}
       </Container>
     </div>
   );
 }
 
 export default App;
+ 
